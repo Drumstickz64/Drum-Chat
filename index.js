94e40069d07c602f5f48828941aa085c7d4d6f67
Drumstickz64 <gearman533@gmail.com>
2020-11-22 01:55:25 +0300

    created pages for creating and joining rooms

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const socketio = require("socket.io");
 
+// constants
 const PRODUCTION = process.env.NODE_ENV === "production";
 const PORT = 5000;
 
+// setup
 const app = express();
+const server = app.listen(PORT, () => console.log("connected on port " + 5000));
+const io = socketio(server);
 
+// middleware
 app.use(morgan(PRODUCTION? "tiny" : "dev"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+io.on("connection", socket => {
+	console.log("yay!");
+});
 
-app.listen(PORT, () => console.log("connected on port " + 5000));
