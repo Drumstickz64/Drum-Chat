140d640cd07e507bc4b012e98ffa0cb3a1a6f7a6
Drumstickz64 <gearman533@gmail.com>
2020-11-23 01:03:39 +0300

    added the abilty the create/join rooms and basic chat functionality

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const socketio = require("socket.io");
 // constants
 const PRODUCTION = process.env.NODE_ENV === "production";
 const PORT = 5000;
+const ROOMS = {};
 
 // setup
 const app = express();
@@ -17,6 +18,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 io.on("connection", socket => {
-	console.log("yay!");
+	console.log(`socket ${socket.id} connected`);
+	
+	socket.on("join-room", roomKey => {
+		socket.join(roomKey);
+		socket.currentPublicRoomKey = roomKey;
+	});
+	
+	socket.on("chat-msg", ({ msg, sender }) => {
+		console.log(`${sender} said ${msg}`);
+		socket.to(socket.currentPublicRoomKey).broadcast.emit("chat-msg", { msg, sender });
+	});
 });
 
+app.post("/create-room", (req, res) => {
+	const name = req.body.name;
+	const roomName = req.body["room-name"];
+	
+	const date = Date.now();
+	const randomNum = Math.random();
+	const roomKey = Math.floor(date * randomNum);
+	ROOMS[roomKey] = req.body["room-name"];
+	
+	res.redirect(`/chat.html?name=${name}&room-name=${roomName}&room-key=${roomKey}`);
+});
+
+app.post("/join-room", (req, res) => {
+	const name = req.body[""];
+	const roomKey = req.body["room-key"];
+	const roomName = ROOMS[roomKey];
+	
+	res.redirect(`/chat.html?name=${name}&room-name=${roomName}&room-key=${roomKey}`);
+});
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,50 @@
 const hamburgerEl = document.querySelector(".hamburger");
 const chatAreaEl = document.querySelector(".chat-area");
 const infoEl = document.querySelector(".info");
+const chatBarEl = document.querySelector(".chat-bar");
+const chatInputEl = document.getElementById("chat-input");
+
+
+const urlParams = new URL(location.href).searchParams;
+const userName = urlParams.get("name");
+const roomName = urlParams.get("room-name");
+const roomKey = urlParams.get("room-key");
+
 
 hamburgerEl.addEventListener("click", () => {
 	infoEl.classList.toggle("hidden");
 	chatAreaEl.classList.toggle("hidden");
+});
+
+const socket = io();
+socket.emit("join-room", roomKey);
+
+const makeChatMsgEl = (msg, sender) => {
+	const chatMsgEl = document.createElement("p");
+	chatMsgEl.classList.add("chat-msg");
+	
+	if (sender) {
+		const senderEl = document.createElement("span");
+		senderEl.classList.add("sender");
+		senderEl.textContent = sender + ": ";
+		chatMsgEl.appendChild(senderEl);
+	} else {
+		chatMsgEl.classList.add("chat-msg-self");
+	}
+	
+	chatMsgEl.appendChild(document.createTextNode(msg));
+	chatAreaEl.appendChild(chatMsgEl);
+};
+
+chatBarEl.addEventListener("submit", e => {
+	e.preventDefault();
+	let msg = chatInputEl.value;
+	makeChatMsgEl(msg);
+	socket.emit("chat-msg", { msg, sender: userName });
+	chatInputEl.value = "";
+});
+
+socket.on("chat-msg", ({ msg, sender }) => {
+	alert("stranger!")
+	makeChatMsgEl(msg, sender);
 });
